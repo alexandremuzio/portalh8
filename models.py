ea072c4477e10f1d812debde11574074fa52b5da
Ilharco <ilharco@ilharco.(none)>
2014-05-14 19:18:53 -0300

    Fixing models

--- models.py
+++ models.py
@@ -1,30 +1,22 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/test.db'
 db = SQLAlchemy(app)
-
-
 class Resident(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nick_name = db.Column(db.String(20))
     name = db.Column(db.String(80))
     graduation_year = db.Column(db.String(10))
     course = db.Column(db.String(30))
-    address = db.relationship('Address', backref='person', 
-                                lazy='dynamic')
-
-
+    address = db.relationship('Address', backref='resident', uselist=False)
     def __init__(self, name, nick_name, course, graduation_year):
         self.nick_name = nick_name
         self.name = name
         self.course = course
         self.graduation_year = graduation_year
-    # def __repr__(self):
-    #     return '<Resident %r : nick_name: %r, graduation_year %r>' % self.username
-    #                                             self.nick_name self.graduation_year
-
+    def __repr__(self):
+        return '<Resident %s : nick_name: %s, course %s, graduation_year %s, id %s>' % (self.username, self.nick_name, self.course, self.graduation_year, self.id)
 
 class Address(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -38,3 +30,6 @@ class Address(db.Model):
         self.apartment = apartment
         self.allocation = allocation
         self.resident_id = resident_id
+
+    def __repr__(self):
+        return '<Quarter %s : Apartment: %s, Allocation %s, Resident Id %s, Id %s>' % (self.quarter, self.apartment, self.allocation, self.resident_id, self.id)
